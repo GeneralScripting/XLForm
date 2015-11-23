1b0071464c1be8827e105b705fbcd9835d25c4ff
Jan Schwenzien <jan@general-scripting.com>
2015-11-23 01:50:29 +0100

    try NSNumberFormatter

--- XLForm/XL/Cell/XLFormTextFieldCell.m
+++ XLForm/XL/Cell/XLFormTextFieldCell.m
@@ -278,7 +278,11 @@ NSString *const XLFormTextFieldLengthPercentage = @"textFieldLengthPercentage";
 - (void)textFieldDidChange:(UITextField *)textField{
     if([self.textField.text length] > 0) {
         if ([self.rowDescriptor.rowType isEqualToString:XLFormRowDescriptorTypeNumber] || [self.rowDescriptor.rowType isEqualToString:XLFormRowDescriptorTypeDecimal]){
-            self.rowDescriptor.value = [[self.textField.text stringByReplacingOccurencesOfString:@”,” withString:@”.”] doublevalue];
+            //self.rowDescriptor.value = [[self.textField.text stringByReplacingOccurencesOfString:@”,” withString:@”.”] doublevalue];
+            NSNumberFormatter * myNumFormatter = [[NSNumberFormatter alloc] init];
+            [myNumFormatter setLocale:[NSLocale currentLocale]];
+            [myNumFormatter setNumberStyle:NSNumberFormatterDecimalStyle];
+            self.rowDescriptor.value =  [myNumFormatter numberFromString:self.textField.text];
         } else if ([self.rowDescriptor.rowType isEqualToString:XLFormRowDescriptorTypeInteger]){
             self.rowDescriptor.value = @([self.textField.text integerValue]);
         } else {
